fabfe0ab85f843c3182f5ca5175d33beff6dda59
gauravagarwal-qait <gaurav.agarwalvi@gmail.com>
2026-08-02 11:03:31 +0530

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM Services -> Client Work navigation', () => {
+  test('should open Client Work page from Services menu', async ({ page }) => {
+    // Test steps will be added in the next update.
+  });
+});
